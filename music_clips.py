9abe76b731bb2d1f6a43b6523ae8ccbb83dbd38c
Bogdan <51471521+chyd1k@users.noreply.github.com>
2019-06-22 11:23:52 +0300

    Add files via upload

---
+++ music_clips.py
@@ -0,0 +1,121 @@
+links_to_clips = [
+'''
+Thirty Seconds To Mars - The Kill (Bury Me)
+https://www.youtube.com/watch?v=8yvGCAvOAfM
+''',
+
+'''
+Linkin Park - Numb
+https://www.youtube.com/watch?v=kXYiU_JCYtU
+''',
+
+'''
+System of a Down - Toxicity
+https://www.youtube.com/watch?v=64FrYGRrKVY
+''',
+
+'''
+Linkin Park - In The End
+https://www.youtube.com/watch?v=eVTXPUF4Oz4
+''',
+
+'''
+System Of A Down - Aerials
+https://www.youtube.com/watch?v=L-iepu3EtyE
+''',
+
+'''
+Three Days Grace - Pain
+https://www.youtube.com/watch?v=Ud4HuAzHEUc
+''',
+
+'''
+Rammstein - Sonne
+https://www.youtube.com/watch?v=StZcUAPRRac
+''',
+
+'''
+Rammstein - Mutter
+https://www.youtube.com/watch?v=gNdnVVHfseA
+''',
+
+'''
+Rammstein - Haifisch
+https://www.youtube.com/watch?v=GukNjYQZW8s
+''',
+
+'''
+Rammstein - Rosenrot
+https://www.youtube.com/watch?v=af59U2BRRAU
+''',
+
+'''
+Rammstein - Deutschland
+https://www.youtube.com/watch?v=NeQM1c-XCDc
+''',
+
+'''
+Rammstein - Ohne Dich
+https://www.youtube.com/watch?v=LIPc1cfS-oQ
+''',
+
+'''
+Rammstein - Mein Herz Brennt, Piano Version
+https://www.youtube.com/watch?v=Dv6Th7kJ64Q
+''',
+
+'''
+Thirty Seconds To Mars - A Beautiful Lie
+https://www.youtube.com/watch?v=4Kvd-uquuhI
+''',
+
+'''
+Linkin Park - Breaking The Habit
+https://www.youtube.com/watch?v=v2H4l9RpkwM
+''',
+
+'''
+Linkin Park - LOST IN THE ECHO
+https://www.youtube.com/watch?v=co4YpHTqmfQ
+''',
+
+'''
+Limp Bizkit - Rollin'
+https://www.youtube.com/watch?v=RYnFIRc0k6E
+''',
+
+'''
+Bullet For My Valentine - Tears Don't Fall
+https://www.youtube.com/watch?v=9sTQ0QdkN3Q
+''',
+
+'''
+Bring Me The Horizon - Drown
+https://www.youtube.com/watch?v=TkV5709EG5M
+''',
+
+'''
+Bring Me The Horizon - Shadow Moses
+https://www.youtube.com/watch?v=-k9qDxyxS3s
+''',
+
+'''
+Bring Me The Horizon - Sleepwalking
+https://www.youtube.com/watch?v=lir3dzYIhz0
+''',
+
+'''
+Bring Me The Horizon - Can You Feel My Heart
+https://www.youtube.com/watch?v=QJJYpsA5tv8
+''',
+
+'''
+Bring Me The Horizon - Throne
+https://www.youtube.com/watch?v=Ow_qI_F2ZJI
+''',
+
+'''
+The Pretty Reckless - Make Me Wanna Die
+https://www.youtube.com/watch?v=txBfhpm1jI0
+'''
+]
